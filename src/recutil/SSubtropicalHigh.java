1673ff1c1acdbb82f1f8eb4a0aa8d05032d12864
liucouhua <liucouhua@163.com>
2019-09-19 22:12:55 +0800

    更改了平滑参数，使得副高轴的长度偏差更小

--- src/recutil/SSubtropicalHigh.java
+++ src/recutil/SSubtropicalHigh.java
@@ -15,6 +15,7 @@ public class SSubtropicalHigh{
         Iterator<Entry<Integer, SystemFeature>> it = features.entrySet().iterator();  
         float lat;
         int ig,jg,oldId;
+        
         //ids.writeToFile("G:/data/systemIdentify/ids0.txt");
         while(it.hasNext()){  
             Entry<Integer, SystemFeature> entry = it.next();  
@@ -36,7 +37,7 @@ public class SSubtropicalHigh{
 		GridData marker = ids.sign01();
 	//	marker.writeToFile("G:/data/systemIdentify/maker.txt");
 		GridData smheight = height.copy();
-		smheight.smooth(15);
+		smheight.smooth(5);
 	//	smheight.writeToFile("G:/data/systemIdentify/smheight.txt");
 		ArrayList<Line> ridge = SystemIdentification.HighValueAreaRidge(tranDirection,smheight,level);  //鑾峰緱鑴婄嚎
 		ridge = LineDealing.cutLines(ridge, marker);
@@ -47,8 +48,13 @@ public class SSubtropicalHigh{
 		tranDirection = SystemIdentification.getDirectionFromLine(ridge, marker);
 		ridge = SystemIdentification.HighValueAreaRidge(tranDirection,smheight,level);
 		ridge = LineDealing.cutLines(ridge, marker);
-		LineDealing.smoothLines(ridge, 30);
+		LineDealing.smoothLines(ridge, 10);
 		ridge = SystemIdentification.getLongLine(scale,ridge);
+		
+		
+		// 从线条的两端沿着风场方向延伸
+		//extend_lines(ridge,height);
+		
 	//	LineDealing.writeToFile("G:/data/systemIdentify/ridge1.txt",ridge);
 		WeatherSystems sh = new  WeatherSystems("SubtropicalHigh",level);//瀹氫箟鍓儹甯﹂珮鍘嬶紝骞惰繘琛岃祴鍊�
 		sh.type= "副高";
@@ -60,4 +66,49 @@ public class SSubtropicalHigh{
 		
 		return sh;
 	}
+	
+
+	private static void extend_lines(ArrayList<Line> jetLine, GridData height) {
+		// TODO Auto-generated method stub
+		
+		//VectorData windm = VectorMathod.rotate(windp, 180);
+		int nline = jetLine.size();
+		float h;
+		float [] ps = new float[2],p1 = new float[2],p_1 = new float[2];
+		for(int i = 0;i <nline;i++) {
+			Line line1 = jetLine.get(i);
+
+			for(int j = 0;j <1000;j++) {
+				h = VectorMathod.getValue(height, ps);
+				if(h < 5880) break;
+				ps = line1.point.get(0);
+				p_1 = line1.point.get(1);
+				p1[0] = 2 * ps[0] - p_1[0];
+				p1[1] = 2 * ps[1] - p_1[1];
+				if(p1[0]<=height.gridInfo.startlon+1 || p1[0] >= height.gridInfo.endlon-1) break;
+				if(p1[1]<=height.gridInfo.startlat+1 || p1[1] >= height.gridInfo.endlat-1) break;
+				line1.point.add(0,p1);
+				
+			}
+			int npoint = line1.point.size();
+			float[] pe = new float[2];
+			for(int j = 0;j <1000;j++) {
+				h = VectorMathod.getValue(height, ps);
+				if(h < 5880) break;
+				pe = line1.point.get(npoint -1);
+				p_1 = line1.point.get(npoint -2);
+				p1[0] = 2 * ps[0] - p_1[0];
+				p1[1] = 2 * ps[1] - p_1[1];
+				if(p1[0]<=height.gridInfo.startlon+1 || p1[0] >= height.gridInfo.endlon-1) break;
+				if(p1[1]<=height.gridInfo.startlat+1 || p1[1] >= height.gridInfo.endlat-1) break;
+				line1.point.add(0,p1);
+			}
+			
+			
+			
+		}
+		
+	}
+	
+	
 }
